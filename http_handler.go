90df72821146e5507d5617a3ba805ff33d81ae9d
takahashi_da_kotaro <kotaro.da.takahashi@drecom.co.jp>
2024-01-18 18:46:34 +0900

    errorハンドリング削除

--- http_handler.go
+++ http_handler.go
@@ -9,12 +9,9 @@ import (
 func MiddlewareFunc(config *BuuurstDevConfig) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			bodyBytes, err := io.ReadAll(r.Body)
+			// Responsibility for error handling is deferred to the parent application.
+			bodyBytes, _ := io.ReadAll(r.Body)
 			r.Body.Close()
-			if err != nil {
-				http.Error(w, "Internal Server Error: Failed to read request", http.StatusInternalServerError)
-				return
-			}
 			r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
 			writer := NewResponseWriterWithStatus(w)
 			next.ServeHTTP(writer, r)
